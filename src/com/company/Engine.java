4fbd9022da3f30d0fe94ccd9c81afb6add64cf99
Mitch <38238516+mrshootems@users.noreply.github.com>
2019-10-29 18:12:07 +1100

    game loop

--- src/com/company/Engine.java
+++ src/com/company/Engine.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Engine {
 
-    public int[] bestMove(Game game1) {
+    public static int[] bestMove(Game game1) {
 
         // TODO:: write an actual evaluation function
 
@@ -12,6 +12,17 @@ public class Engine {
         int[] result = {position, confidence};
 
         return result;
+    }
+
+    public static int[] perft(Game game1, int depth) {
+
+        // TODO:: this method lmao
+        // if I'm lazy, just dump something that looks like my eval function that just makes it wait and
+        // spit out something roughly in the class of n^2
+
+        int[] dummy_do_not_use = {depth,depth*2};
+
+        return dummy_do_not_use;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -7,6 +9,55 @@ public class Main {
         // TODO:: map to coordinator control flow
         Game thisgame = new Game();
         thisgame.initialiseBoard();
+
+        // game loop setup
+        boolean running = true;
+        Scanner sc = new Scanner(System.in);
+
+
+        // game loop
+        while (running) {
+            String cinput = sc.nextLine();
+
+            if (cinput.equals("quit")) {
+                System.out.println("quitting");
+                running = false;
+            }
+
+            else if (cinput.equals("name")) {
+                System.out.println("engine_name");
+            }
+
+            else if (cinput.equals("isready")) {
+                // TODO:: engine.ready() or something like that
+                System.out.println("readyok");
+            }
+
+            else if (cinput.contains("position startpos")) {
+                thisgame.updateMovesList(cinput.substring(18));
+            }
+
+            else if (cinput.contains("go ftime")) {
+                // TODO:: set up space delimited thing (??)
+                int[] times = new int[2];
+
+                int i = 9;
+                while (!(cinput.charAt(i) == ' ')){ i++; } // find the index for the space following x
+                times[0] = Integer.parseInt(cinput.substring(9,(i-1))); // determine x
+                times[1] = Integer.parseInt(cinput.substring(i+7)); // we know how far after the end of x the start if y is
+
+                int[] thisMove = Engine.bestMove(thisgame);
+                System.out.println("bestmove " + thisMove[0] + " " + thisMove[1]);
+            }
+
+            else if (cinput.contains("perft")) {
+                int x = Integer.parseInt(cinput.substring(6));
+                int[] thisPerft = Engine.perft(thisgame,x);
+                System.out.println("bestmove " + thisPerft[0] + " " + thisPerft[1]);
+            }
+
+
+        }
     }
 
 
